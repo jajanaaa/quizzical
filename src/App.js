38799aa2af747960f12f29bf237f401da55576b8
janamanzana <96806525+jajanaaa@users.noreply.github.com>
2022-10-27 01:41:40 +0200

    Working on functionality

--- src/App.js
+++ src/App.js
@@ -1,57 +1,125 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import QuestionBlock from "./QuestionBlock";
+// import { v4 as uuidv4 } from "uuid";
+
+// function App() {
+//   const [questions, setQuestions] = useState([]); // questions its array of 5 objects
+//   const [choosedAnswer, setChoosedAnswer] = useState({});
+
+//   function showQuestions(response) {
+//     setQuestions(response.data.results);
+//   }
+
+//   function chooseAnswer(event) {
+//     console.log(event.target.checked);
+//     // setChoosedAnswer((prevAnswer) => ({
+//     //   ...prevAnswer,
+//     //   [event.target.name]: event.target.value,
+//     // }));
+//     setChoosedAnswer({
+//       [event.target.name]: event.target.value,
+//       isChoosen: event.target.checked,
+//     });
+//   }
+//   const newQuestions = questions.map((question) => {
+//     return (
+//       <>
+//         <QuestionBlock
+//           question={question.question}
+//           correct={question.correct_answer}
+//           incorrect={question.incorrect_answers}
+//           id={uuidv4()}
+//           key={uuidv4()}
+//           fun={chooseAnswer}
+//           name={questions.indexOf(question)}
+//           checked={choosedAnswer.isChoosen}
+//         />
+//         <hr />
+//       </>
+//     );
+//   });
+
+//   useEffect(() => {
+//     axios
+//       .get("https://opentdb.com/api.php?amount=5&type=multiple")
+//       .then(showQuestions);
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <>{newQuestions}</>
+//       <button>Check answers</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import QuestionBlock from "./QuestionBlock";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [questions, setQuestions] = useState([]); // questions its array of 5 objects
-  const [choosedAnswer, setChoosedAnswer] = useState({});
+  const [results, setResults] = useState([]);
 
-  function showQuestions(response) {
-    setQuestions(response.data.results);
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api.php?amount=5&type=multiple")
+      .then((response) => setResults(response.data.results));
+  }, []);
+  //
+  const [choseAnswers, setChoseAnswers] = useState([{}]);
+
+  function checkAnswers() {
+    const correct = results.map((result) => result.correct_answer);
+
+    for (let i = 0; i < correct.length; i++) {
+      if (
+        choseAnswers.value === correct[i] &&
+        choseAnswers.isChecked === true
+      ) {
+        console.log("Correct!");
+        setChoseAnswers((prevAnswer) => ({
+          ...prevAnswer,
+          isCorrect: true,
+        }));
+      } else console.log("Wrong");
+    }
   }
 
-  function chooseAnswer(event) {
-    console.log(event.target.checked);
-    // setChoosedAnswer((prevAnswer) => ({
-    //   ...prevAnswer,
-    //   [event.target.name]: event.target.value,
-    // }));
-    setChoosedAnswer({
-      [event.target.name]: event.target.value,
-      isChoosen: event.target.checked,
-    });
+  function handleChange(event) {
+    setChoseAnswers((prevAnswer) => ({
+      ...prevAnswer,
+      value: event.target.value,
+      isChecked: event.target.checked,
+    }));
   }
-  console.log(choosedAnswer);
-  const newQuestions = questions.map((question) => {
+
+  //
+  const renderedResults = results.map((result) => {
     return (
       <>
         <QuestionBlock
-          question={question.question}
-          correct={question.correct_answer}
-          incorrect={question.incorrect_answers}
-          id={uuidv4()}
-          key={uuidv4()}
-          fun={chooseAnswer}
-          name={questions.indexOf(question)}
-          checked={choosedAnswer.isChoosen}
+          question={result.question}
+          answers={result.incorrect_answers
+            .concat([result.correct_answer])
+            .sort(() => 0.5 - Math.random())}
+          correct={result.correct_answer}
+          name={results.indexOf(result)}
+          fun={handleChange}
+          isCorrect={choseAnswers.isCorrect}
         />
         <hr />
       </>
     );
   });
 
-  useEffect(() => {
-    axios
-      .get("https://opentdb.com/api.php?amount=5&type=multiple")
-      .then(showQuestions);
-  }, []);
-
   return (
     <div className="App">
-      <>{newQuestions}</>
-      <button>Check answers</button>
+      {renderedResults}
+      <button onClick={checkAnswers}>Check</button>
     </div>
   );
 }
--- src/QuestionBlock.js
+++ src/QuestionBlock.js
@@ -1,42 +1,116 @@
-import React, { useState, useEffect } from "react";
-// import parse from "html-react-parser";
+// import React, { useState, useEffect } from "react";
+// // import parse from "html-react-parser";
+
+// function QuestionBlock(props) {
+//   // const id = props.id;
+//   const [answers, setAnswers] = useState([]);
+//   //   const parsee = parse(props.question);
+//   const incorrect = props.incorrect;
+//   const correct = props.correct;
+
+//   // setAnswers(incorrect.concat([correct]).sort(() => 0.5 - Math.random()));
+//   useEffect(() => {
+//     setAnswers(incorrect.concat([correct]));
+//   }, [correct, incorrect]);
+
+//   const answersButtons = answers.map((answer) => {
+//     return (
+//       <>
+//         <input
+//           type="radio"
+//           id={answer}
+//           // name={id}
+//           name={props.name}
+//           value={answer}
+//           // onClick={props.fun}
+//           // choosen={props.checked}
+//           onClick={props.fun}
+//         />
+//         <label htmlFor={answer} className={props.checked ? "green" : null}>
+//           {answer}
+//         </label>
+//       </>
+//     );
+//   });
+//   return (
+//     <>
+//       {props.question}
+//       {answersButtons}
+//     </>
+//   );
+// }
+
+// export default QuestionBlock;
+
+import React, { useState } from "react";
 
 function QuestionBlock(props) {
-  // const id = props.id;
-  const [answers, setAnswers] = useState([]);
-  console.log(answers);
-  //   const parsee = parse(props.question);
-  const incorrect = props.incorrect;
-  const correct = props.correct;
+  //   function chooseAnswer(event) {
+  //     // setChoosedAnswer((prevAnswer) => ({
+  //     //   ...prevAnswer,
+  //     //   [event.target.name]: event.target.value,
+  //     // }));
+  //     setChoosedAnswer({
+  //       [event.target.name]: event.target.value,
+  //       isChoosen: event.target.checked,
+  //     });
+  //   }
+  // const [choseAnswers, setChoseAnswers] = useState([{}]);
 
-  // setAnswers(incorrect.concat([correct]).sort(() => 0.5 - Math.random()));
-  useEffect(() => {
-    setAnswers(incorrect.concat([correct]));
-  }, [correct, incorrect]);
+  // function checkAnswers() {
+  //   if (
+  //     choseAnswers.value === props.correct &&
+  //     choseAnswers.isChecked === true
+  //   ) {
+  //     console.log("Correct!");
+  //   }
+  // }
 
-  const answersButtons = answers.map((answer) => {
+  // checkAnswers();
+
+  // function handleChange(event) {
+  //   setChoseAnswers((prevAnswer) => ({
+  //     ...prevAnswer,
+  //     value: event.target.value,
+  //     isChecked: event.target.checked,
+  //   }));
+  // }
+
+  // const renderAnswers = props.answers.map((answer) => {
+  //   return (
+  //     <>
+  //       <input
+  //         type="radio"
+  //         id="huey"
+  //         name={props.name}
+  //         value={answer}
+  //         // checked={props.name === answer}
+  //         onChange={handleChange}
+  //       />
+  //       <label htmlFor="huey">{answer}</label>
+  //     </>
+  //   );
+  // });
+  const renderAnswers = props.answers.map((answer) => {
     return (
       <>
         <input
           type="radio"
-          id={answer}
-          // name={id}
+          id="huey"
           name={props.name}
           value={answer}
-          // onClick={props.fun}
-          // choosen={props.checked}
-          onClick={props.fun}
+          // checked={props.name === answer}
+          onChange={props.fun}
+          // className={ ? "green" : null}
         />
-        <label htmlFor={answer} className={props.checked ? "green" : null}>
-          {answer}
-        </label>
+        <label htmlFor="huey">{answer}</label>
       </>
     );
   });
   return (
     <>
-      {props.question}
-      {answersButtons}
+      <p>{props.question}</p>
+      {renderAnswers}
     </>
   );
 }
